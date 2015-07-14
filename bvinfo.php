eb9454b4b7999403bb6e0796ff0060ad3272f47a
yair lahav <yair@billrun.net>
2015-07-14 08:14:03 +0300

    draf realease

--- bvinfo.php
+++ bvinfo.php
@@ -19,18 +19,21 @@ require_once('geoplugin.php');
 class PlgSystemBvinfo extends JPlugin {
 
 	var $debug = false;
-	var $geoplugin;
+	var $geoInplugin;
 
-    public function PlgSystemBvinfo(&$subject, $config)
-	{
+    public function PlgSystemBvinfo(&$subject, $config) {
         if ($this->debug) {
-			echo "<br>     PlgSystemBvinfo -  PlgSystemBvinfo<br>";
-		}
-		$this->geoplugin = new geoPlugin();
-		echo "<br>		--- geoplugin --------------------------<br>";
-		//prinr_r($this->geoplugin);
-		parent::__construct($subject, $config);
+        	echo "<br>     PlgSystemBvinfo -  PlgSystemBvinfo<br>";
 	}
+	$this->geoplugin = new geoPlugin();
+			$this->geoplugin->locate();
+        	if ($this->debug) {
+			echo "<br>		--- geoplugin --------------------------<br>";
+			print_r($this->geoplugin);
+		}
+
+	parent::__construct($subject, $config);
+    }
 
 	/**
 	 * Gets visitor information.
@@ -41,36 +44,9 @@ class PlgSystemBvinfo extends JPlugin {
 	 */
 	public function onGetVisitorInfo()
 	{
-		echo "<br>     PlgSystemBvinfo -  onGetVisitorInfo<br>";
-		$info->ip = $this->getVisitorIp();
-		$ipInfo = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$info->ip));
-        echo "<br>     PlgSystemBvinfo -  after getIp<br>";
-        print_r ($ipInfo);
-		echo "<br>		-----------------------------<br>";
-		$this->geoplugin->locate();
-		echo "<br>		--- geoplugin --------------------------<br>";
-		// prinr_r($this->geoplugin);
-		return $ipInfo;
+        	if ($this->debug) {
+			echo "<br>     PlgSystemBvinfo -  onGetVisitorInfo<br>";
+		}
+		return $this->geoplugin;
 	}
-
-	function getVisitorIp()
-     {
-          $ipaddress = '';
-          if (getenv('HTTP_CLIENT_IP'))
-              $ipaddress = getenv('HTTP_CLIENT_IP');
-          else if(getenv('HTTP_X_FORWARDED_FOR'))
-              $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-          else if(getenv('HTTP_X_FORWARDED'))
-              $ipaddress = getenv('HTTP_X_FORWARDED');
-          else if(getenv('HTTP_FORWARDED_FOR'))
-              $ipaddress = getenv('HTTP_FORWARDED_FOR');
-          else if(getenv('HTTP_FORWARDED'))
-              $ipaddress = getenv('HTTP_FORWARDED');
-          else if(getenv('REMOTE_ADDR'))
-              $ipaddress = getenv('REMOTE_ADDR');
-          else
-              $ipaddress = 'UNKNOWN';
-
-          return $ipaddress;
-     }
 }
--- geoplugin.php
+++ geoplugin.php
@@ -58,7 +58,21 @@ class geoPlugin {
 		global $_SERVER;
 
 		if ( is_null( $ip ) ) {
-			$ip = $_SERVER['REMOTE_ADDR'];
+            if (getenv('HTTP_CLIENT_IP')) {
+                $ip = getenv('HTTP_CLIENT_IP');
+            } else if(getenv('HTTP_X_FORWARDED_FOR')) {
+                $ip = getenv('HTTP_X_FORWARDED_FOR');
+            } else if(getenv('HTTP_X_FORWARDED')) {
+              $ip = getenv('HTTP_X_FORWARDED');
+            } else if(getenv('HTTP_FORWARDED_FOR')) {
+              $ip = getenv('HTTP_FORWARDED_FOR');
+            } else if(getenv('HTTP_FORWARDED')) {
+              $ip = getenv('HTTP_FORWARDED');
+            } else if(getenv('REMOTE_ADDR')) {
+              $ip = getenv('REMOTE_ADDR');
+            } else {
+              $ip = 'UNKNOWN';
+            }
 		}
 
 		$host = str_replace( '{IP}', $ip, $this->host );
